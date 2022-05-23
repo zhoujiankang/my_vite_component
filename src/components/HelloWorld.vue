e7fb44b370faceb01c6b39433ed8c4c5cc9419bb
jackzhou <jackzhou@yeahka.com>
2022-05-23 15:25:33 +0800

    初始化公共组件库

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+    <el-button type="primary" @click="increment">{{ state.count }}</el-button>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue';
+defineProps<{ msg: string }>();
+// 增加按钮
+const state = reactive({count: 0});
+const increment = () => {
+    state.count ++;
+};
+</script>
+
+<style scoped>
+a {
+    color: #c9227e;
+}
+
+label {
+    margin: 0 0.5em;
+    font-weight: bold;
+}
+
+code {
+    background-color: #eee;
+    padding: 2px 4px;
+    border-radius: 4px;
+    color: #304455;
+}
+</style>
